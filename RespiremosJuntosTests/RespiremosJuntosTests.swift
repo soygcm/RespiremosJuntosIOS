4ff1507f3f8d1890cdfc9f1b8b4ec1e8dfd7769d
Gabriel Castañaza <gabriel@MacBook-Pro-de-Gabriel.local>
2015-02-10 14:02:27 -0600

    Initial Commit

---
+++ RespiremosJuntosTests/RespiremosJuntosTests.swift
@@ -0,0 +1,36 @@
+//
+//  RespiremosJuntosTests.swift
+//  RespiremosJuntosTests
+//
+//  Created by Gabriel Castañaza on 10/2/15.
+//  Copyright (c) 2015 Ministerio de Salud. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class RespiremosJuntosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
