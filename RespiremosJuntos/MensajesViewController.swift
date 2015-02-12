dd26c680f14c90266540f67b46c7188d2d96c115
soygcm <gcmelmejor@gmail.com>
2015-02-12 16:41:52 -0600

    Revert b2ecd9f..fc4da04
    This rolls back to commit b2ecd9fb9e505abb25e43f80212b6304f9350085.

--- RespiremosJuntos/MensajesViewController.swift
+++ RespiremosJuntos/MensajesViewController.swift
@@ -10,14 +10,98 @@ import UIKit
 
 class MensajesViewController: UITableViewController {
     
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     let mensajes = [
         "Gracias por instalar esta wevada",
         "DEJA DE FUMAR YA!!!!",
         "si sigues fumando vas a quedar como esta imagen *imagen horrible de las consecuencias de fumar*"
     ]
+    
+    
+    @IBAction func loginButton(sender: AnyObject) {
+        
+        // Set permissions required from the facebook user account
+        let permissionsArray = [ "user_about_me", "user_relationships", "user_birthday", "user_location"]
+    
+        
+        PFFacebookUtils.logInWithPermissions(permissionsArray, block: { (user: PFUser!, error: NSError!) -> Void in
+            
+            
+            self.activityIndicator.stopAnimating()
+            
+            if user == nil {
+                var errorMessage = ""
+                if error == nil{
+                    NSLog("Oh oh, el usuario cancelo el Facebook login.")
+                    errorMessage = "Oh oh, el usuario cancelo el Facebook login."
+                }else{
+                    NSLog("Uh oh. An error occurred: %@", error)
+                    errorMessage = error.localizedDescription
+                }
+                
+                var alert = UIAlertView(title: "Error", message: errorMessage, delegate: nil, cancelButtonTitle: nil, otherButtonTitles: "Dismiss")
+                alert.show()
+                
+            }else{
+                
+                self.userDetails()
+                
+                if user.isNew {
+                    NSLog("User with facebook signed up and logged in!")
+                }else{
+                    NSLog("User with facebook logged in!")
+                }
+                
+            }
+            
+            
+        })
+        
+        self.activityIndicator.startAnimating()
+
+        
+    }
+    
+    func userDetails() {
+        
+        let request = FBRequest.requestForMe()
+        request.startWithCompletionHandler { (connection: FBRequestConnection!, result: AnyObject!, error: NSError!) -> Void in
+            
+            if error == nil{
+                
+                let userData = result as NSDictionary
+                
+                let facebookID = userData["id"] as String
+                let name = userData["name"] as String
+                let location = userData["location"] as NSDictionary
+                let locationName = location["name"] as String
+                let gender = userData["gender"] as String
+                let birthday = userData["birthday"] as String
+                let relationship = userData["relationship_status"] as String
+                
+                let pictureURL = NSURL(string: "https://graph.facebook.com/\(facebookID)/picture?type=large&return_ssl_resources=1")
+
+                NSLog("fbID: \(facebookID) name: \(name) birthday: \(birthday) ")
+                
+                
+            }
+            
+        }
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+     
+        
+        if PFUser.currentUser() != nil &&
+            PFFacebookUtils.isLinkedWithUser(
+            PFUser.currentUser()) {
+                
+                userDetails()
+                
+        }
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
